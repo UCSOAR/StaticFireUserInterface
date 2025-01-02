42c8853074fb12f93917725c95f01f70ed60b5d3
Kailey <kaileykobar@gmail.com>
2025-01-02 16:19:24 -0600

    Your commit message

--- temp/RocketControlUnitGUI/test/db_create.py
+++ temp/RocketControlUnitGUI/test/db_create.py
@@ -1,26 +1,34 @@
-from pocketbase import Client
-from pocketbase.models.collection import collection
+from pocketbase import PocketBase
+from pocketbase.models.collection import Collection
 
 def create_table(client, name, data):
-    client.collections.create()
-    # give it a name
-    name : string = name
-    data : number = data
+    """
+    Creates a collection in PocketBase.
+
+    """
+    name : str = name
+    data : int = data
     #create a collection
     try:
-        collection = client.collections.create(client)
-        print(f"collection {string} add something here {number}.")
+        collection_data = {
+        "name": name,
+        "data": data,
+        }
+        collection = client.collections.create(collection_data)
+        print(f"collection '{name}' add something here '{data}'.")
     except Exception as e:
         print(f"error has occured: {e}")
-        #
-def main():
-    client = client('http://127')
-    123 = [
-        {"name": "something", "data": "something", "required": True},
-        {"name": "something", "data": "something", "required": False}
+        
+def main():    
+    client = PocketBase('http://127.0.0.1:8090')
+    authData = client.collection("_superusers").auth_with_password(admin_username, admin_password)
+
+    something = [
+        {"name": "something", "type":  "something", "required": True},
+        {"name": "something", "type": "something", "required": False}
     ]
 
-    create_table(client, "TEST_TABLE", 123)
+    create_table(client, "TEST_TABLE", something)
 
 
 if __name__ == "__main__":
