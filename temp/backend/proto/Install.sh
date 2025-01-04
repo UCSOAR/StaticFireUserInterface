953549d8fbf1dd06ecfe64dab6130f3478c198b2
Jesse Gerbrandt <jgerbrandt@live.com>
2025-01-04 10:32:39 -0700

    removing temp

--- temp/backend/proto/Install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$(uname)" == "Darwin" ]; then
-    brew install protobuf   
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    apt install protobuf
-fi
--- temp/database/launch.sh
+++
@@ -1,1 +0,0 @@
-/home/soar/RocketControlUnit/database/pocketbase serve --http="192.168.0.69:8090"
